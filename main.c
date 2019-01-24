d60a4b80a561f52c62b0950a650f92c025b93388
Elijah Ward <elijahward.dev@gmail.com>
2019-01-24 12:06:02 -0500

    initialize variables for required args

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
 // main.c
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char **argv) {
-    int idx;
+    int flags, opts;
+    int type, height, width, format;
+    char **output_name;
+
     for ( idx=0; idx < argc; idx++){
         puts(argv[idx]);
     }
